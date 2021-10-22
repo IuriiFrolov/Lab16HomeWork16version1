b65b02b42236feb5de951a1a09d41e328b0a69b6
IuriiFrolov <fym95@yandex.ru>
2021-10-22 15:40:01 +0300

    Добавьте файлы проекта.

---
+++ Lab16HomeWork16version1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Text.Json.Serialization;
+using System.IO;
+
+namespace Lab16HomeWork16version1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Users/Фролов Юрий/source/repos/Lab16HomeWork16version1/Lab16HomeWork16version1/bin/Debug/Product/Product.json"; // Путь
+            if (!File.Exists(path)) // Содаем файл
+            {File.Create(path);}
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(path))   // Записываем в файл
+                {
+                    try
+                    {
+                        string[] arrayJsonString = new string[5];// НЕ ЗАБЫТЬ исправить на 5
+                        for (int i = 0; i < 5; i++) // НЕ ЗАБЫТЬ исправить на 5
+                        {
+                            #region Ввод переменных
+                            Console.Write("Введите код товара: ");
+                            int code = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Введите название товара: ");
+                            string name = Convert.ToString(Console.ReadLine());
+                            Console.Write("Введите стоимость товара: ");
+                            double prise = Convert.ToDouble(Console.ReadLine());
+                            #endregion
+
+                            Product product = new Product() //Обращение к классу
+                            {
+                                Code = code,
+                                Name = name,
+                                Price = prise,
+                            };//Обращение к классу
+                            JsonSerializerOptions options = new JsonSerializerOptions() //Опции. Добавляем Кирилицу и пробелы
+                            {
+                                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                                //WriteIndented = true  
+                            };//Опции. Добавляем Кирилицу и пробелы
+                            arrayJsonString[i] = JsonSerializer.Serialize(product, options); //Сериализация 
+                            sw.WriteLine(arrayJsonString[i]);
+                            Console.WriteLine();
+                        }
+                    }
+                    catch (Exception ex) { Console.WriteLine(ex.Message); }
+                }
+                //sw.Close();
+                Console.WriteLine("Запись в файл Product.json произведена.");
+                Console.ReadKey();
+            }
+        }
+    }
+    class Product
+    {
+        [JsonPropertyName("Код товара")]
+        public int Code { get; set; }
+        [JsonPropertyName("Название товара")]
+        public string Name { get; set; }
+        [JsonPropertyName("Цена товара")]
+        public double Price { get; set; }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Text.Json.Serialization;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = "C:/Users/Фролов Юрий/source/repos/Lab16HomeWork16version1/Lab16HomeWork16version1/bin/Debug/Product/Product.json";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string[] arrayJsonString = new string[5];
+                double priceMax = 0;
+                string priceMaxName = "";
+                for (int i = 0; i < 5; i++)
+                {
+                    arrayJsonString[i] = sr.ReadLine();
+                    Product product1 = JsonSerializer.Deserialize<Product>(arrayJsonString[i]); //Десериализация 
+                    Console.WriteLine("Код товара: {0}    Название товара: {1,8}    Цена товара: {2,5:f2}", product1.Code, product1.Name,product1.Price);
+                    if (priceMax< product1.Price)
+                    {
+                        priceMaxName = product1.Name;
+
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine("Самый дорогой товар: {0,8}", priceMaxName);
+            }
+            Console.ReadKey();
+        }
+    }
+    class Product
+    {
+        [JsonPropertyName("Код товара")]
+        public int Code { get; set; }
+        [JsonPropertyName("Название товара")]
+        public string Name { get; set; }
+        [JsonPropertyName("Цена товара")]
+        public double Price { get; set; }
+    }
+}
